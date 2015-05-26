9009d38932a6b83d8d94d7714d5131490c12eb29
Icylu <Icylu@Icylu-PC>
2015-05-26 23:27:57 +0800

    first

---
+++ clone/src/Clone.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Icylu
+ */
+public class Clone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello github.");
+        System.out.println("aha,it all that i will say to you.");
+    }
+
+}
