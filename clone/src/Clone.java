06d295c28b9afa4215b22f0be29bd35a11f2cc68
Icylu <Icylu@Icylu-PC>
2015-05-26 23:43:19 +0800

    add if atatmet

--- clone/src/Clone.java
+++ clone/src/Clone.java
@@ -11,6 +11,10 @@ public class Clone {
     public static void main(String[] args) {
         System.out.println("hello github.");
         System.out.println("aha,it all that i will say to you.");
+        int a=3,b=4;
+        if (a>b) {
+            System.out.println("a>b");
+        }
     }
 
 }
